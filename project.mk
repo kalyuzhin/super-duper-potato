b56be8de30751f15957db238381ce8459b297675
kalyuzhin <67906018+kalyuzhin@users.noreply.github.com>
2026-01-31 22:25:39 +0300

    [PSWD] – add make file

---
+++ project.mk
@@ -0,0 +1,36 @@
+include .env
+export $(shell sed 's/=.*//' .env)
+
+DB_SETUP := user=$(DB_USERNAME) password=$(DB_PASSWORD) dbname=$(DB_NAME) host=$(DB_HOST) port=$(DB_PORT) sslmode=disable
+MIGRATIONS_DIR = scripts/migrations
+
+.PHONY: help
+## prints help about all targets
+help:
+	@echo ""
+	@echo "Usage:"
+	@echo "  make <target>"
+	@echo ""
+	@echo "Targets:"
+	@awk '                                \
+		BEGIN { comment=""; }             \
+		/^\s*##/ {                         \
+		    comment = substr($$0, index($$0,$$2)); next; \
+		}                                  \
+		/^[a-zA-Z0-9_-]+:/ {               \
+		    target = $$1;                  \
+		    sub(":", "", target);          \
+		    if (comment != "") {           \
+		        printf "  %-17s %s\n", target, comment; \
+		        comment="";                \
+		    }                              \
+		}' $(MAKEFILE_LIST)
+	@echo ""
+
+.PHONY: build
+build:
+	go build -o bin/main cmd/passwordmanager/main.go
+
+.PHONY: migrate
+migrate:
+	goose -dir "$(MIGRATIONS_DIR)" postgres "$(DB_SETUP)" up
